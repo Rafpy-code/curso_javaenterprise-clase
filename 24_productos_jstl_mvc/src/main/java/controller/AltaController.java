dc5d367e6f80480f5657c90fdc4d2b3dddee7ef3
tarde <tarde@A13E11>
2024-01-09 18:18:51 +0100

    mvc inicios

---
+++ 24_productos_jstl_mvc/src/main/java/controller/AltaController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Producto;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AltaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		List<Producto> productos = new ArrayList<>();
+
+		if (session.getAttribute("carrito") != null) {
+			productos = (List<Producto>) session.getAttribute("carrito");
+		}
+
+		String nombre = request.getParameter("nombre");
+		double precio = 0;
+		try {
+			precio = Double.parseDouble(request.getParameter("precio"));
+		} catch (Exception e) {
+			System.out.println("Formato incorrecto, use el '.' para separar los decimales!");
+		}
+		String categoria = request.getParameter("categoria");
+
+		System.out.println(nombre + " | " + precio + " | " + categoria);
+
+		productos.add(new Producto(nombre, precio, categoria));
+
+		session.setAttribute("carrito", productos);
+
+	}
+
+}
