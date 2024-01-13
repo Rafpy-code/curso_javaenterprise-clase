d46151967305cdfdfe950228dd3f66d99aa99ebb
rafpy <castor.ardilla.me@gmail.com>
2024-01-13 10:57:02 +0100

    28 registro de clientes con jpa

---
+++ 28_registro_clientes_bd/src/main/java/model/Cliente.java
@@ -0,0 +1,73 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idCliente;
+	private String nombre;
+	private String apellido;
+	@Column(name = "forma_pago")
+	private String formaPago;
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(long idCliente, String nombre, String apellido, String formaPago) {
+		super();
+		this.idCliente = idCliente;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.formaPago = formaPago;
+	}
+
+	public long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getFormaPago() {
+		return formaPago;
+	}
+
+	public void setFormaPago(String formaPago) {
+		this.formaPago = formaPago;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [idCliente=" + idCliente + ", nombre=" + nombre + ", apellido=" + apellido + ", formaPago="
+				+ formaPago + "]";
+	}
+	
+	
+}
