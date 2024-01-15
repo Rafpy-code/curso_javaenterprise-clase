087392a29a6127f0c5994457b6e08ef0a835c1e2
tarde <tarde@A13E11>
2024-01-15 20:56:24 +0100

    salto de l

--- 28_cursos_web_bd/src/main/java/controller/ModificarDuracionController.java
+++ 28_cursos_web_bd/src/main/java/controller/ModificarDuracionController.java
@@ -14,6 +14,7 @@ public class ModificarDuracionController extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
 		CursosService service = new CursosService();
 
 		service.modificarDuracion(request.getParameter("nombre"), Integer.parseInt(request.getParameter("duracion")));
